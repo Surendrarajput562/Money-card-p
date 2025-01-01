7e2c2909abe5a842969f956c4a0c86a6367dc551
Surendrarajput562 <rajputsurendra562@gmail.com>
2025-01-01 22:15:20 +0530

    Update index.js
    Port

--- index.js
+++ index.js
@@ -180,7 +180,7 @@ app.get("/", (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;  // 3000 से 4000 बदल दिया है
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
